dd287d47311b7b1b89cd4abb71e5b0c7304b946c
johan57211 <jyucrat@unsa.edu.pe>
2025-11-03 16:52:41 -0500

    corrigiendo ejercicio04

--- ejercicio04.js
+++ ejercicio04.js
@@ -1,5 +1,6 @@
 let num = Number(prompt("Ingrese un numero:"));
 
 console.log("Su cuadrado: "+Math.pow(num, 2)+
+            "Su cubo: "+Math.pow(num, 3)+
             "\nSu raiz: "+Math.sqrt(num, 2)
 );
