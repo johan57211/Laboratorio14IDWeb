85c9afd0ebf4a9b88eb1614ede7266422056b566
johan57211 <jyucrat@unsa.edu.pe>
2025-11-03 16:12:42 -0500

    ejercicio01.js

---
+++ ejercicio01.js
@@ -0,0 +1,6 @@
+let num = Number(prompt("Ingrese un numero decimal:"));
+
+console.log("Redondeo hacia abajo: " + Math.floor(num));
+console.log("Redondeo hacia arriba: " + Math.ceil(num));
+console.log("Redondeo normal: "+Math.round(num));
+
